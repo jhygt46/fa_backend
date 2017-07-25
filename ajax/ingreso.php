c02bf7ab116aa42eb3f2cc24551e375b85c14b67
jhygt46 <infolifeoperacional@gmail.com>
2017-07-25 14:20:31 -0400

    aa

--- ajax/ingreso.php
+++ ajax/ingreso.php
@@ -1,20 +1,36 @@
 <?php
 session_start();
 
+header('Access-Control-Allow-Origin: *');
 header('Content-type: text/json');
 header('Content-type: application/json');
 
+$path = $_SERVER['DOCUMENT_ROOT'];
 if($_SERVER['HTTP_HOST'] == "localhost"){
-    $path = $_SERVER['DOCUMENT_ROOT']."/";
-}else{
-    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/";
 }
-$path_ = $path."admin/class";
 
 
-require_once($path_."/ingreso_class.php");
-$ingreso = new Ingreso();
-$info = $ingreso->ingresar_user();
+
+if($tipo == "app"){
+    
+    $postdata = file_get_contents("php://input");
+    $email = $postdata->email;
+    $pass = $postdata->pass;
+    $tipo = $postdata->tipo;
+    
+    $info['id'] = 5;
+    $info['id_cue'] = 1;
+    $info['token'] = "abo9aP115dFmX6iu29";
+    
+}else{
+    
+    $path_ = $path."admin/class";
+    require_once($path_."/ingreso_class.php");
+    $ingreso = new Ingreso();
+    $info = $ingreso->ingresar_user();
+    
+}
 echo json_encode($info);
 
 ?>
