2aac247ad0d00c09cc12d091ba6a7a86d00da853
jhygt46 <infolifeoperacional@gmail.com>
2017-08-23 14:56:00 -0300

    aa

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
         exit;
     }
     
+    $to = "diegomez13@hotmail.com";
+    $subject = "Recuperar Password Fireapp";
+    $message = "<html><head><title>FireApp</title></head><body><a href='http://www.fireapp.cl/admin/password.php?id=".$id."&code=".$code."'>PRESIONAR ACA</a></body></html>";
+    // To send HTML mail, the Content-type header must be set
+    $headers[] = "MIME-Version: 1.0";
+    $headers[] = "Content-type: text/html; charset=iso-8859-1";
+
+    if(mail($to, $subject, $message, implode("\r\n", $headers))){
+        echo "SI<br>";
+    }else{
+        echo "NO<br>";
+    }
+    
     phpinfo();
     exit;
     
