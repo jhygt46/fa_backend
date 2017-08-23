c73b1d4c1517a1554df8bc46544f40da3d72e846
jhygt46 <infolifeoperacional@gmail.com>
2017-08-22 22:41:09 -0300

    aa

--- ajax/ingreso.php
+++ ajax/ingreso.php
@@ -9,10 +9,16 @@ $path = $_SERVER['DOCUMENT_ROOT'];
 if($_SERVER['HTTP_HOST'] == "localhost"){
     $path_class .= "/fa_backend/class";
     $path_n .= "/fa_backend";
+    $path .= "/";
 }else{
     $path_class .= "admin/class";
     $path_n .= "admin";
 }
+
+echo $path_class."<br>";
+echo $path_n."<br>";
+echo $path."<br>";
+exit;
 
 require_once($path_class."/ingreso_class.php");
 $ingreso = new Ingreso();
