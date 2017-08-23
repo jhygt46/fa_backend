3eaa7507be774d842d5e793363a30c505b04b5a0
jhygt46 <infolifeoperacional@gmail.com>
2017-08-22 23:45:30 -0300

    aa

--- ajax/index.php
+++ ajax/index.php
@@ -7,9 +7,15 @@ header('Content-type: application/json');
 $path = $_SERVER['DOCUMENT_ROOT'];
 if($_SERVER['HTTP_HOST'] == "localhost"){
     $path .= "/";
+    $path_class = $path."/fa_backend/class/";
+    $path_n = $path."/fa_backend/";
+    
+}else{
+    $path_class = $path."admin/class/";
+    $path_n = $path."admin/";
 }
-$path_ = $path."fa_backend/class";
-require_once($path_."/guardar.php");
+
+require_once($path_class."guardar.php");
 $guardar = new Guardar();
 $data = $guardar->process();
 echo json_encode($data);
