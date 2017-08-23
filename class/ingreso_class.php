d66ae1d123c6b9102d538eb18e98c3ca1f00aa36
jhygt46 <infolifeoperacional@gmail.com>
2017-08-23 16:23:11 -0300

    aa

--- class/ingreso_class.php
+++ class/ingreso_class.php
@@ -39,7 +39,7 @@ class Ingreso {
                 $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
                 $code = substr(str_shuffle($chars), 0, 30);
                 $this->con->sql("UPDATE usuarios SET code='".$code."' WHERE id_user='".$user["resultado"][0]["id_user"]."'");
-                $url = "http://www.jardinvalleencantado.cl/send_fireapp.php?id=".$id."&correo=".$correo."&code=".$code;
+                $url = "http://www.jardinvalleencantado.cl/send_fireapp.php?id=".$user['resultado'][0]['id_user']."&correo=".$user['resultado'][0]['correo']."&code=".$code;
                 $a = file_get_contents($url);
                 $info["op"] = 1;
                 $info["message"] = $a;
