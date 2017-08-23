039bfaab70a08281c7fdf907792aaf3cb0902bdd
jhygt46 <infolifeoperacional@gmail.com>
2017-08-23 17:36:07 -0300

    aa

--- js/password.js
+++ js/password.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
             data: "accion=recuperar_password&id="+id+"&code="+code+"&pass1="+pass1+"&pass2="+pass2,
             success: function(data){
                 
+                console.log(data);
                 if(data.op == 1){
                     
                     $('.msg').html("Se ha creado su Contrase&ntilde;a con Exito");
