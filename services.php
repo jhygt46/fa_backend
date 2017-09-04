9d224bf2ecb601376654b232e9cdbfcd6b711842
jhygt46 <infolifeoperacional@gmail.com>
2017-09-04 15:02:04 -0300

    aa

--- services.php
+++ services.php
@@ -4,9 +4,13 @@ session_start();
 header('Content-type: text/json');
 header('Content-type: application/json');
 
+echo "1";
 require_once 'class/services_class.php';
+echo "2";
 $services = new Services();
+echo "3";
 $res = $services->process();
+echo "4";
     
 echo json_encode($res);
     
