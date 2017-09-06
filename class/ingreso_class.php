b0a523962d3e44c891f5cac2a209c131c4c7fd50
jhygt46 <infolifeoperacional@gmail.com>
2017-09-06 00:26:31 -0300

    aa

--- class/ingreso_class.php
+++ class/ingreso_class.php
@@ -37,7 +37,7 @@ class Ingreso {
         if(filter_var($_POST['user'], FILTER_VALIDATE_EMAIL)){
             
             $user = $this->con->sql("SELECT * FROM usuarios WHERE correo='".$_POST["user"]."' AND eliminado='0'");
-            $info['db'] = $user;
+            
             if($user['count'] == 1){
                 
                 $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -78,6 +78,7 @@ class Ingreso {
         if(filter_var($_POST['user'], FILTER_VALIDATE_EMAIL)){
             
             $user = $this->con->sql("SELECT * FROM usuarios WHERE correo='".$_POST['user']."' AND eliminado='0'");
+            $info['db'] = $user;
             if($user['count'] == 0){
                 // CORREO NO SE ENCUENTERA EN LA BASE DE DATOS
                 $info['op'] = 2;
