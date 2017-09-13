d7ee18c9d635c4a66d225c147eb788ca13671801
jhygt46 <infolifeoperacional@gmail.com>
2017-09-13 18:45:13 -0300

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -105,7 +105,9 @@ class Services extends Core{
                 
                 $carros = $this->con->sql("SELECT t1.id_car, t2.nombre FROM actos_carros t1, carros t2 WHERE t1.id_act='".$lis_actos[$i]['id_act']."' AND t1.id_car=t2.id_car");
                 if($carros['count'] > 0){
+                    
                     for($j=0; $j<$carros['count']; $j++){
+                        
                         $aux_carros['id_car'] = $carros['resultado'][$j]['id_car'];
                         $aux_carros['nombre'] = $carros['resultado'][$j]['nombre'];
                         $aux_carros['id_cia'] = $carros['resultado'][$j]['id_cia'];
@@ -115,9 +117,19 @@ class Services extends Core{
                         $aux_carros['63'] = date("Y-m-d h:i:s");
                         $aux_carros['69'] = date("Y-m-d h:i:s");
                         $aux_carros['610'] = date("Y-m-d h:i:s");
+                        
+                        $aux_pos_carros['id'] = $carros['resultado'][$j]['id_user'];
+                        $aux_pos_carros['lat'] = $carros['resultado'][$j]['lat'];
+                        $aux_pos_carros['lng'] = $carros['resultado'][$j]['lng'];
+                        $aux_pos_carros['icon'] = 1;
+                        
+                        $aux['pos'][] =  $aux_pos_carros;
                         //$aux['info']['carros'][] = $aux_carros;
                         unset($aux_carros);
+                        unset($aux_pos_carros);
+                        
                     }
+                    
                 }
 
                 $voluntarios = $this->con->sql("SELECT t2.id_user, t2.nombre, t2.id_cia, t2.id_cue FROM actos_user t1, usuarios t2 WHERE t1.id_act='".$lis_actos[$i]['id_act']."' AND t1.id_user=t2.id_user");
