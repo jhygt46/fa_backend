a1c7e3d194100269334caef8fa235b8c4a8bd27e
jhygt46 <infolifeoperacional@gmail.com>
2017-09-16 17:39:36 -0300

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -80,8 +80,7 @@ class Services extends Core{
                 $aux['info']['direccion'] = $lis_actos[$i]['direccion'];
                 $aux['info']['lat'] = $lis_actos[$i]['lat'];
                 $aux['info']['lng'] = $lis_actos[$i]['lng'];
-                $aux['info']['fecha'] = strtotime($lis_actos[$i]['fecha_creado']);
-                $aux['info']['fecha2'] = time();
+                $aux['info']['fecha'] = time() - strtotime($lis_actos[$i]['fecha_creado']);
                 $aux['info']['fecha_fin'] = 0;
                 $aux['info']['id_cue'] = $lis_actos[$i]['id_cue'];
                 
