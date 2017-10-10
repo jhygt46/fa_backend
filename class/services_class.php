7d69fd8fb8be573c93f0743f71e0fa996e0e3584
jhygt46 <infolifeoperacional@gmail.com>
2017-10-10 18:06:44 -0300

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -294,7 +294,7 @@ class Services extends Core{
         $id_cue = 1;
         $id_act = 1;
         
-        $users = $this->con->sql("SELECT t1.id_user, t1.nombre, t2.id_act FROM usuarios t1 LEFT JOIN actos_user t2 ON t1.id_user=t2.id_user AND t1.id_cia='1'");
+        $users = $this->con->sql("SELECT t1.id_user, t1.nombre, t2.id_act FROM usuarios t1 LEFT JOIN actos_user t2 ON t1.id_user=t2.id_user AND t1.id_cia='1' AND t1.id_cue='1'");
         
         echo "<pre>";
         print_r($users);
