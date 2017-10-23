073dac16b1627a1041a1d3d26b00194e66fff2ce
jhygt46 <infolifeoperacional@gmail.com>
2017-10-23 17:32:18 -0300

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -78,7 +78,7 @@ class Services extends Core{
         }
         
         $fecha = date("Y-m-d h:i:s", strtotime("-15 day"));
-        $actos = $this->con->sql("SELECT t1.id_act, t2.nombre, t2.clave, t1.direccion, t1.lat, t1.lng, t1.fecha_creado, t1.id_cue FROM actos t1, claves t2 WHERE t1.fecha_creado >= '".$fecha."' AND t1.id_cla=t2.id_cla AND (t2.tipo=1 OR t2.tipo=2)");
+        $actos = $this->con->sql("SELECT t1.id_act, t2.nombre, t2.clave, t1.direccion, t1.lat, t1.lng, t1.fecha_creado, t1.id_cue FROM actos t1, claves t2 WHERE t1.fecha_creado >= '".$fecha."' AND t1.id_cla=t2.id_cla AND (t2.tipo=1 OR t2.tipo=2) AND t1.id_act!=1");
         
         if($actos['count'] > 0){
             $lis_actos = $actos['resultado'];
