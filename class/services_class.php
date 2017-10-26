bf317737283b36bd8b2dd77fd877aa9562fe68ba
jhygt46 <infolifeoperacional@gmail.com>
2017-10-26 18:26:34 -0300

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -289,8 +289,6 @@ class Services extends Core{
                 }
             }
             
-            $aux['info']['sql'] = $cias;
-            
             $carros = $this->con->sql("SELECT t2.id_car, t2.nombre, t2.id_cia, t2.id_cue, t2.id_user, t1.cantidad, t2.lat, t2.lng FROM actos_carros t1, carros t2 WHERE t1.id_act='".$acto['resultado'][0]['id_act']."' AND t1.id_car=t2.id_car");
             if($carros['count'] > 0){
                 for($j=0; $j<$carros['count']; $j++){
@@ -318,7 +316,7 @@ class Services extends Core{
                 }
                 $aux['info']['maquinas'] = implode(" ", $infomaquinas);
             }
-            $aux['info']['grifos'] = $this->getgrifos($lis_actos[$i]['lat'], $lis_actos[$i]['lng']);
+            $aux['info']['grifos'] = $this->getgrifos($acto['resultado'][0]['lat'], $acto['resultado'][0]['lng']);
             return $aux;
             
         }
