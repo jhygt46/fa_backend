c72de29a0523cbae6becf765bff8ec447fa246d6
jhygt46 <infolifeoperacional@gmail.com>
2018-01-23 16:31:33 -0300

    aa

--- ajax/create.php
+++ ajax/create.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header('Content-type: text/json');
 header('Content-type: application/json');
 
-
+echo "1";
 
 $path = $_SERVER['DOCUMENT_ROOT'];
 if($_SERVER['HTTP_HOST'] == "localhost"){
@@ -15,10 +15,10 @@ if($_SERVER['HTTP_HOST'] == "localhost"){
     $path_class = $path."admin/class/";
     $path_n = $path."admin/";
 }
-
+echo "2";
 require_once($path_class."guardar.php");
 $guardar = new Guardar();
-
+echo "3";
 if($_POST['accion'] == "crear_cuerpo_pagina"){
     $data = $guardar->crear_cuerpo_pagina();
     echo json_encode($data);
