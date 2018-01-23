3447990e30cc4e67c20d201450b4aeec5867d18c
jhygt46 <infolifeoperacional@gmail.com>
2018-01-23 17:57:56 -0300

    aa

--- ajax/create.php
+++ ajax/create.php
@@ -6,18 +6,12 @@ header('Content-type: application/json');
 
 $path = $_SERVER['DOCUMENT_ROOT'];
 if($_SERVER['HTTP_HOST'] == "localhost"){
-    $path .= "/";
-    $path_class = $path."fa_backend/class/";
-    $path_n = $path."fa_backend/";
-}else{
-    $path_class = $path."admin/class/";
-    $path_n = $path."admin/";
+    $path .= "/fa_bakend/";
 }
 
 $info['path'] = $path;
-$info['path_class'] = $path_class;
-$info['path_n'] = $path_n;
 print_r($info);
+exit;
 
 require_once($path_class."guardar.php");
 $guardar = new Guardar();
