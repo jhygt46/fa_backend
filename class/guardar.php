85937c1a65f8eb26dc94967dc7bdcdc5af1a638e
jhygt46 <infolifeoperacional@gmail.com>
2018-01-25 16:38:51 -0300

    aa

--- class/guardar.php
+++ class/guardar.php
@@ -984,8 +984,8 @@ class Guardar extends Core{
             $info['time'] = $time;
             $info['aux_time'] = $aux_time;
             
-            $info['fecha_time'] = date("dd-mm-YY H:i:s", $time);
-            $info['fecha_aux_time'] = date("dd-mm-YY H:i:s", $aux_time);
+            $info['fecha_time'] = date("d-m-Y H:i:s", $time);
+            $info['fecha_aux_time'] = date("d-m-Y H:i:s", $aux_time);
             
             if($aux < 0){
                 
