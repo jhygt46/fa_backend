2f1dded7c07bef44ddadd9f67cb6a6d220b46126
jhygt46 <infolifeoperacional@gmail.com>
2018-04-13 16:22:01 -0300

    aa

--- ajax/login_app.php
+++ ajax/login_app.php
@@ -16,6 +16,13 @@ if($_SERVER['HTTP_HOST'] == "localhost"){
 
 require_once($path_class."login_class.php");
 $login = new Login();
-$data = $ingreso->login_app();
+
+if($_POST['accion'] == "login"){
+    $data = $ingreso->login_app();
+}
+if($_POST['accion'] == "recuperar"){
+    $data = $login->enviar_clave();
+}
+
 echo json_encode($data);
 
