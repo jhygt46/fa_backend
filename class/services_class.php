0b172b71d0b219f76570151d7c89553a9cc194cd
jhygt46 <infolifeoperacional@gmail.com>
2018-04-25 17:17:46 -0300

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -613,16 +613,12 @@ class Services extends Core{
     private function setasistencia($id_act, $id_user, $asist){
         
         $aux = $this->con->sql("SELECT * FROM actos_user WHERE id_act='".$id_act."' AND id_user='".$id_user."'");
-        $info['asist'] = $asist;
-        $info['count'] = $aux['count'];
         
         if($aux['count'] == 0 && $asist == 1){
             $this->con->sql("INSERT INTO actos_user (id_act, id_user) VALUES ('".$id_act."', '".$id_user."')");
-            $info['in'] = 1;
         }
         if($aux['count'] == 1 && $asist == 2){
             $this->con->sql("DELETE FROM actos_user WHERE id_act='".$id_act."' AND id_user='".$id_user."'");
-            $info['in'] = 2;
         }
         
         return $info;
