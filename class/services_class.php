bc86e87b0f7ee0735da07c448fdf51dd4033dd09
jhygt46 <infolifeoperacional@gmail.com>
2018-05-18 23:22:58 -0400

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -768,14 +768,14 @@ class Services extends Core{
                     $inform = $this->con->sql("SELECT * FROM informe WHERE id_act='".$id_act."' AND id_cia='".$id_cia."'");
                     $autos = json_decode($inform['resultado'][0]['autos']);
                     $info['tipo'] = 'tipo: '.$tipo;
-                    /*
+                    
                     if($tipo == "autos"){
                         
-                        $i = $data['i'];
-                        $autos[$i]['patente'] = $data['patente'];
-                        $autos[$i]['marca'] = $data['marca'];
-                        $autos[$i]['modelo'] = $data['modelo'];
-                        return $this->con->sql("UPDATE informe SET autos='".json_encode($autos)."' WHERE id_act='".$id_act."' AND id_cia='".$id_cia."'");
+                        $m = $data['i'];
+                        $autos[$m]['patente'] = $data['patente'];
+                        $autos[$m]['marca'] = $data['marca'];
+                        $autos[$m]['modelo'] = $data['modelo'];
+                        //return $this->con->sql("UPDATE informe SET autos='".json_encode($autos)."' WHERE id_act='".$id_act."' AND id_cia='".$id_cia."'");
                         
                     }
                     if($tipo == "lesionados"){
@@ -787,7 +787,7 @@ class Services extends Core{
                         return $this->con->sql("UPDATE informe SET autos='".json_encode($autos)."' WHERE id_act='".$id_act."' AND id_cia='".$id_cia."'");
                         
                     }
-                    */
+                    
                 }
                 
             }
