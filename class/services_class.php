b3a4f11aa0b6f6c91d7110c74944fd7a1284ca73
jhygt46 <infolifeoperacional@gmail.com>
2018-05-19 00:06:07 -0400

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -774,7 +774,7 @@ class Services extends Core{
                     if($tipo == "autos"){
 
                         $m = intval($data['i']);
-                        //$autos[$m]['patente'] = $data['patente'];
+                        $autos[$m]['patente'] = $data['patente'];
                         //$autos[$m]['marca'] = $data['marca'];
                         //$autos[$m]['modelo'] = $data['modelo'];
                         //$this->con->sql("UPDATE informe SET autos='".json_encode($autos)."' WHERE id_act='".$id_act."' AND id_cia='".$id_cia."'");
