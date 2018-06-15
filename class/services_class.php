8650d3e26fc4fcd521628128670182858f1ae8e3
jhygt46 <infolifeoperacional@gmail.com>
2018-06-15 16:22:49 -0400

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -138,7 +138,7 @@ class Services extends Core{
                     }
                 }
                 
-                $carros = $this->con->sql("SELECT t2.id_car, t2.nombre, t2.id_cia, t2.id_cue, t1.cantidad, t2.lat, t2.lng FROM actos_carros t1, carros t2 WHERE t1.id_act='".$lis_actos[$i]['id_act']."' AND t1.id_car=t2.id_car");
+                $carros = $this->con->sql("SELECT t2.id_car, t2.nombre, t2.id_cia, t2.id_cue, t2.lat, t2.lng FROM actos_carros t1, carros t2 WHERE t1.id_act='".$lis_actos[$i]['id_act']."' AND t1.id_car=t2.id_car");
                 $aux['info']['buena'] = $carros;
                 if($carros['count'] > 0){
                     for($j=0; $j<$carros['count']; $j++){
@@ -319,7 +319,7 @@ class Services extends Core{
                 }
             }
             
-            $carros = $this->con->sql("SELECT t2.id_car, t2.nombre, t2.id_cia, t2.id_cue, t1.cantidad, t2.lat, t2.lng FROM actos_carros t1, carros t2 WHERE t1.id_act='".$acto['resultado'][0]['id_act']."' AND t1.id_car=t2.id_car");
+            $carros = $this->con->sql("SELECT t2.id_car, t2.nombre, t2.id_cia, t2.id_cue, t2.lat, t2.lng FROM actos_carros t1, carros t2 WHERE t1.id_act='".$acto['resultado'][0]['id_act']."' AND t1.id_car=t2.id_car");
             if($carros['count'] > 0){
                 for($j=0; $j<$carros['count']; $j++){
                     $infomaquinas[] = $carros['resultado'][$j]['nombre'];
@@ -327,6 +327,7 @@ class Services extends Core{
                     $aux_carros['nombre'] = $carros['resultado'][$j]['nombre'];
                     $aux_carros['id_cia'] = $carros['resultado'][$j]['id_cia'];
                     $aux_carros['id_cue'] = $carros['resultado'][$j]['id_cue'];
+                    
                     /*
                     if($carros['resultado'][$j]['id_user'] != 0){    
                         $aux_pos['id_user'] = $carros['resultado'][$j]['id_user'];
@@ -340,7 +341,7 @@ class Services extends Core{
                         unset($aux_pos);
                     }
                     */
-                    $aux_carros['cantidad'] = $carros['resultado'][$j]['cantidad'];
+                    
                     $aux['info']['carros'][] = $aux_carros;
                     unset($aux_carros);
                 }
