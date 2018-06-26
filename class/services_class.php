0dd89d87a3c27aabd5b7e8fb854c6ebaf269ab21
jhygt46 <infolifeoperacional@gmail.com>
2018-06-25 22:59:19 -0400

    aa

--- class/services_class.php
+++ class/services_class.php
@@ -532,7 +532,7 @@ class Services extends Core{
         $in = $this->verificar_code($id_user, $code, true);
         if($in['op'] == 1){
             
-            $actos = $this->con->sql("SELECT t2.tipo, t1.id_act, t2.clave, t2.nombre, t1.direccion, t3.id_user FROM (actos t1, claves t2) LEFT JOIN actos_user t3 ON t1.id_act=t3.id_act AND t3.id_user='".$id_user."' WHERE t1.id_cla=t2.id_cla AND t2.asist='1' AND t1.id_cue='".$in['user']['id_cue']."'");
+            $actos = $this->con->sql("SELECT t2.tipo, t1.id_act, t2.clave, t2.nombre, t1.direccion, t3.id_user FROM (actos t1, claves t2) LEFT JOIN actos_user t3 ON t1.id_act=t3.id_act AND t3.id_user='".$id_user."' WHERE t1.id_cla=t2.id_cla AND t2.asist='1' AND t1.id_cue='".$in['user']['id_cue']."' AND (t1.id_cia='".$in['user']['id_cia']."' OR (t1.id_cia='0' AND t2.iscia='0'))");
             for($i=0; $i<$actos['count']; $i++){
             
                 $aux['id'] = $actos['resultado'][$i]['id_act'];
